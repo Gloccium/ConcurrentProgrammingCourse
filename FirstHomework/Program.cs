bae7c83ca5077435c6d0d891a5ac8778206a5e51
Gloccium <kurururunku@gmail.com>
2022-11-09 20:29:02 +0500

    first homework done

--- FirstHomework/Program.cs
+++ FirstHomework/Program.cs
@@ -8,64 +8,48 @@ namespace FirstHomework
 {
     internal static class Program
     {
-        private static void DoWorkload()
-        {
-            for (var i = 0; i < 300; ++i)
-                i.GetHashCode();
-        }
-        
-        public static void Main()
+        private static Stopwatch _stopwatch;
+
+        private static void Main()
         {
             var process = Process.GetCurrentProcess();
-            process.ProcessorAffinity = (IntPtr)Math.Pow(2, Environment.ProcessorCount - 1);
+            var lastProcessorCore = (IntPtr) Math.Pow(2, Environment.ProcessorCount - 1);
             process.PriorityClass = ProcessPriorityClass.RealTime;
+            process.ProcessorAffinity = lastProcessorCore;
+
+            const int dataCapacity = 10;
+            var data = new List<long>(dataCapacity);
             
-            var data = new List<Tuple<int, long>>();
-            
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
-            
-            var firstThread = new Thread(() =>
+            MeasureTime(data);
+            Console.WriteLine($"The average time is: {data.Average()}");
+        }
+
+        private static void MeasureTime(ICollection<long> data)
+        {
+            for (var i = 0; i < 50; i++)
             {
-                for (var i = 0; i < 100000; i++)
+                _stopwatch = new Stopwatch();
+
+                var firstThread = new Thread(() => {_stopwatch.Start(); while (_stopwatch.IsRunning) { } })
                 {
-                    lock (data)
-                    {
-                        DoWorkload();
-                        data.Add(Tuple.Create(1, stopwatch.ElapsedMilliseconds));
-                    }
-                }
-            });
-            
-            var secondThread = new Thread(() =>
-            {
-                for (var i = 0; i < 100000; i++)
+                    IsBackground = true,
+                    Priority = ThreadPriority.Normal,
+                };
+                firstThread.Start();
+
+
+                var secondThread = new Thread(_stopwatch.Stop)
                 {
-                    lock (data)
-                    {
-                        DoWorkload();
-                        data.Add(Tuple.Create(2, stopwatch.ElapsedMilliseconds));
-                    }
-                }
-            });
-            
-            firstThread.Start();
-            secondThread.Start();
-            firstThread.Join();
-            secondThread.Join();
-            
-            var count = 1;
-            long sum = 0;
-            long temp = 0;
-            var threadsCount = 1;
-            foreach (var item in data.Where(item => item.Item1 != threadsCount))
-            {
-                threadsCount = item.Item1;
-                count++;
-                sum += (item.Item2 - temp);
-                temp = item.Item2;
+                    IsBackground = true,
+                    Priority = ThreadPriority.Highest,
+                };
+                secondThread.Start();
+
+                secondThread.Join();
+                firstThread.Join();
+
+                data.Add(_stopwatch.ElapsedMilliseconds);
             }
-            Console.WriteLine(sum / count);
         }
     }
 }
